175df04b67a5aeb61257befff98933ebca813d55
Aidan <aidanwebster987@gmail.com>
2024-09-12 02:42:49 -0500

    .mtl file parser finished (decided to do it tonight instead)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
 import { App } from "./control/app";
+import { MtlMaterial } from "./view/mtl_material";
+
+// Testing
+
+// const new_mtl_material = new MtlMaterial();
+// new_mtl_material.initialize("dist/models/zammy_monk_with_mtl/zammymonk_with_mtl.mtl")
+
+// console.log(new_mtl_material);
 
 const canvas : HTMLCanvasElement = <HTMLCanvasElement> document.getElementById("gfx-main");
 
--- src/view/mtl_material.ts
+++ src/view/mtl_material.ts
@@ -1,7 +1,8 @@
 import { vec3 } from "gl-matrix"
 
-// This is a material definition that is defined by a .mtl file
-export class MtlMaterial {
+// This is a material definition within the .mtl file
+type SubMaterial = {
+    matId: string
     Ns: number
     Ka: vec3
     Kd: vec3
@@ -10,20 +11,19 @@ export class MtlMaterial {
     Ni: number
     d: number
     illum: number
+}
+
+// Contains a list of submaterials that are defined within a .mtl file
+export class MtlMaterial {
+    
+    sub_materials: SubMaterial[]
 
     constructor() {
-        this.Ns = 0;
-        this.Ka = vec3.create();
-        this.Kd = vec3.create();
-        this.Ks = vec3.create();
-        this.Ke = vec3.create();
-        this.Ni = 0
-        this.d = 0
-        this.illum = 0
+        this.sub_materials = [];
     }
     // I need to construct a GPUTexture from the stuff provided in the .mtl file
 
-    async initialize(device: GPUDevice, file_path: string) {
+    async initialize(file_path: string, device?: GPUDevice, ) {
 
         await this.parse_mtl_file(file_path);
 
@@ -31,7 +31,94 @@ export class MtlMaterial {
 
     async parse_mtl_file(file_path: string) {
 
-        
+        var result: SubMaterial = {
+            matId: "",
+            Ns: 0,
+            Ka: vec3.create(),
+            Kd: vec3.create(),
+            Ks: vec3.create(),
+            Ke: vec3.create(),
+            Ni: 0,
+            d: 0,
+            illum: 0,
+        };
 
+        const response = await fetch(file_path);
+        const blob: Blob = await response.blob();
+        const text = await blob.text();
+        const lines = text.split("\n");
+
+        lines.forEach(
+            line => {
+                const identifiers = line.slice(0, 2).split('');
+
+                if (identifiers[0] == 'n' && identifiers[1] == 'e') {
+                    result.matId = this.read_in_string(line);
+                } else if (identifiers[0] == 'N' && identifiers[1] == 's') {
+                    result.Ns = this.read_in_number(line);
+                } else if (identifiers[0] == 'K' && identifiers[1] == 'a') {
+                    result.Ka = this.read_in_vec3(line);
+                } else if (identifiers[0] == 'K' && identifiers[1] == 'd') {
+                    result.Kd = this.read_in_vec3(line);
+                } else if (identifiers[0] == 'K' && identifiers[1] == 's') {
+                    result.Ks = this.read_in_vec3(line);
+                } else if (identifiers[0] == 'K' && identifiers[1] == 'e') {
+                    result.Ke = this.read_in_vec3(line);
+                } else if (identifiers[0] == 'N' && identifiers[1] == 'i') {
+                    result.Ni = this.read_in_number(line);
+                } else if (identifiers[0] == 'd') {
+                    result.d = this.read_in_number(line);
+                } else if (identifiers[0] == 'i' && identifiers[1] == 'l') {
+                    result.illum = this.read_in_number(line);
+                } else if (identifiers.length == 0 && result.matId != "") {
+                    this.sub_materials.push(result);
+                    result = this.reset_sub_material(result);
+                }
+            }
+        )
+    }
+
+    reset_sub_material(to_reset: SubMaterial): SubMaterial{
+        to_reset = {
+            matId: "",
+            Ns: 0,
+            Ka: vec3.create(),
+            Kd: vec3.create(),
+            Ks: vec3.create(),
+            Ke: vec3.create(),
+            Ni: 0,
+            d: 0,
+            illum: 0,
+        }
+
+        return to_reset;
+    }
+
+    read_in_string(line: string): string {
+        const components = line.split(' ');
+        // component identifier, string
+        const new_string = components[1];
+
+        return new_string;
+    }
+
+    read_in_number(line: string): number{
+        const components = line.split(' ');
+        // component identifier, number
+        const new_number = Number(components[1]).valueOf();
+
+        return new_number;
+    }
+
+    read_in_vec3(line: string): vec3 {
+        const components = line.split(' ');
+        // component identifier, float, float, float
+        const new_vec3: vec3 = [
+            Number(components[1]).valueOf(),
+            Number(components[2]).valueOf(),
+            Number(components[3]).valueOf(),
+        ]
+
+        return new_vec3;
     }
 }
